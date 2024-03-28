09606792c77c55c110341b8b3705ca4507b73c81
Maverish96 <realrishabh9@gmail.com>
2024-03-28 16:29:01 +0530

    yyyyy

---
+++ bananas/src/main.rs
@@ -0,0 +1,12 @@
+
+#[warn(unused_variables)]
+fn main() {
+    let banana = 'a';
+    let x = "abc";
+    let y = x;
+    let arr = [1,2,3];
+    let mango = arr[4]; 
+    println!("{}", x)
+
+    
+}
---
+++ target/debug/bananas.d
@@ -0,0 +1,1 @@
+E:\COde\ work\Rust\Rust\ Practice\target\debug\bananas.exe: E:\COde\ work\Rust\Rust\ Practice\bananas\src\main.rs

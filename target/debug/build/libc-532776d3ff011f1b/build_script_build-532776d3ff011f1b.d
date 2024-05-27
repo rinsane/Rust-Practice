514f9f055e882dfcfca5ad0ca6520cb636585bbf
Maverish96 <realrishabh9@gmail.com>
2024-05-27 12:51:39 +0530

    first one from linux!

---
+++ target/debug/build/libc-532776d3ff011f1b/build_script_build-532776d3ff011f1b.d
@@ -0,0 +1,5 @@
+/media/rira/Extra/COde work/Rust/Rust Practice/target/debug/build/libc-532776d3ff011f1b/build_script_build-532776d3ff011f1b: /home/rira/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs
+
+/media/rira/Extra/COde work/Rust/Rust Practice/target/debug/build/libc-532776d3ff011f1b/build_script_build-532776d3ff011f1b.d: /home/rira/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs
+
+/home/rira/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs:

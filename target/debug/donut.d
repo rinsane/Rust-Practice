969695bf8b78d85368a9169d7d250fcc8a888a27
Maverish96 <realrishabh9@gmail.com>
2024-05-27 23:19:57 +0530

    new start

--- target/debug/donut.d
+++
@@ -1,1 +0,0 @@
-E:\COde\ work\Rust\Rust\ Practice\target\debug\donut.exe: E:\COde\ work\Rust\Rust\ Practice\donut\src\main.rs
---
+++ target/debug/play.d
@@ -0,0 +1,1 @@
+/media/rira/Extra/COde\ work/Rust/Rust\ Practice/target/debug/play: /media/rira/Extra/COde\ work/Rust/Rust\ Practice/play/src/main.rs
